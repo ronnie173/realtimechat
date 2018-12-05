a6b76299bb99c155dc831aa2b05519ff2610eacd
Jerome Raymond <ronnie173@gmail.com>
2018-12-04 21:13:08 -0600

    Added login screen

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import {BsDropdownModule} from 'ngx-bootstrap';
+import { LoginComponent } from './pages/login/login.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BsDropdownModule.forRoot()
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
